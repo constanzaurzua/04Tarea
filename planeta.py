026edc555e15a0160fe73fea8a6c95b53c0026cc
constanzaurzua <urzuacisterna@gmail.com>
2015-10-20 17:05:38 -0300

    ecuacion de mov y euler

--- planeta.py
+++ planeta.py
@@ -26,8 +26,12 @@ class Planeta(object):
         primer orden.
         '''
         x, y, vx, vy = self.y_actual
-        # fx = ...
-        # fy = ...
+        r= (x**2 + y**2)**0.5
+        G=1
+        M=1
+        m=1
+        fx = -G*m*M*(1/r**2 - 2*self.alpha/r**3)*(x / r)
+        fy = -G*m*M*(1/r**2 - 2*self.alpha/r**3)*(y/ r)
         return [vx, vy, fx, fy]
 
     def avanza_euler(self, dt):
@@ -36,12 +40,21 @@ class Planeta(object):
         en un intervalo de tiempo dt usando el método de Euler explícito. El
         método no retorna nada, pero re-setea los valores de self.y_actual.
         '''
+        vx,vy,fx,fy=self.ecuacion_de_movimiento()
+        x1=vx + dt*self.y_actual[0]
+        y1=vy + dt*self.y_actual[1]
+        vx1=fx + dt*self.y_actual[2]
+        vy1=fy + dt*self.y_actual[3]
+        c_actual= [x1,y1,vx1,vy1]
+        self.y_actual=c_actual
+        self.t_actual+=dt
         pass
 
     def avanza_rk4(self, dt):
         '''
         Similar a avanza_euler, pero usando Runge-Kutta 4.
         '''
+        
         pass
 
     def avanza_verlet(self, dt):
@@ -55,5 +68,3 @@ class Planeta(object):
         Calcula la enérgía total del sistema en las condiciones actuales.
         '''
         pass
-
-
